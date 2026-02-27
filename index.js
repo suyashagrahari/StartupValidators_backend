034bd7175a745ce32d807a7bcc48bff371f8aa21
suyashagrahari <suyashagrahari73@gmail.com>
2026-02-27 18:29:07 +0530

    fix

--- index.js
+++ index.js
@@ -86,6 +86,15 @@ const corsOptions = {
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 
+// Hard CORS headers for all domains/subdomains.
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") return res.sendStatus(204);
+  next();
+});
+
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
 app.use(express.json());
